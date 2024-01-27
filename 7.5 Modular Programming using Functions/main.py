aef36921dd1c4e00b5ef977f2e83ba7909cca7b1
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-27 10:56:46 +0000

    Add files via upload

---
+++ 7.5 Modular Programming using Functions/main.py
@@ -0,0 +1,13 @@
+# ------------------------------------------
+# Title: Modular Programming using Functions
+# Date:
+# ------------------------------------------
+
+
+def add_Numbers(param1, param2):
+  sum = param1 + param2
+  print(sum)
+
+
+add_Numbers(1,2)
+
