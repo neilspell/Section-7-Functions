5cffca7295e9bca9b1c77ebb6ad3ab3db583929e
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-27 10:47:45 +0000

    Update main.py

--- 7.3 Iteration - Nested Loops/main.py
+++ 7.3 Iteration - Nested Loops/main.py
@@ -4,33 +4,15 @@
 # ---------------------------
 
 # Task 1
-'''print("*****")
-print("*****")
-print("*****")
-print("*****")'''
+
 
 
 #Task 2.1 & 2.2
-'''cols = 5
-rows = 4
 
-for _rowIndex in range(rows):
-  print()
-  for _colIndex in range(cols):
-    print("*", end = "")'''
+
 
 # Task 3
 
-timesTable = 2
-
-for i in range(1,13):
-  answer = i * timesTable
-  print(timesTable, "x", i, "=", answer)
 
 
 #Task 3.1
-for t_Tables in range(1,13):
-  for i in range(1,13):
-    answer = i * t_Tables
-    print(t_Tables, "x", i, "=", answer)
-  print()
